f94c7bf2d6e3b3ed92ea793af49251e4f65fcd02
Rodrigo Catalan <rcatalan@Austranet.com>
2021-11-30 23:37:38 -0300

    se agrega libreria body parser
    Signed-off-by: Rodrigo Catalan <rcatalan@Austranet.com>

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const axios = require('axios')
+var parser = require('body-parser')
+
+const app = express()
+const port = 3000
+ 
+// parse application/x-www-form-urlencoded
+app.use(parser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(parser.json())
+
+
+
+app.get('/', async (req, res) => {
+  const {data} = await axios.get('https://jsonplaceholder.typicode.com/users/1/todos')
+  res.send(data)
+})
+
+
+
+app.post('/', async (req, res) => {
+  const {data} = await axios.post('https://jsonplaceholder.typicode.com/users/1/todos')
+  res.send(data)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
